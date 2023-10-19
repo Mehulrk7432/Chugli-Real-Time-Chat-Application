a212d58f3924bd77c452bd8119c5a998b01476e7
Mehul <80029739+Mehulrk7432@users.noreply.github.com>
2023-10-19 16:57:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,6 @@ const Message = require("./models/Message");
 const rooms = ["general", "tech", "finance", "crypto"];
 const cors = require("cors");
 
-const app = express()
-app.use(cors(
-  {
-    origin: [""],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-  ));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
